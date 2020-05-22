358dfec5617ec7a0ce344bf79e2024d8ae721472
Damisa Tangprasobsang <damisatangprasobsang@Damisas-MacBook-Pro.local>
2020-05-22 08:45:37 +0700

    New design

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,8 +8,8 @@
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;600&display=swap');
 
 #app {
-  background-color:  #C6D2E3;
-  background-image: linear-gradient(180deg, #D6E0FC 0%, #EAF2F8 50.35%, #FEF9E7);
+  background-color:  #CFD4EC;
+  background-image: linear-gradient(180deg, #CFD4EC 0%, #EAF2F8 50.35%, #FEF9E7);
   font-family: 'Poppins', sans-serif;
   right: 0;
   bottom: 0;
